b6b1d888b36a1bf965382eec663aa7f44f6ceb32
Pisuth D <pisuth.dae@gmail.com>
2020-12-08 19:33:47 +0700

    Feat: Calculate mock ratio

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,8 @@
 export const shortAddress = (address) => {
     return `${address.slice(0,7)}...${address.slice(-5)}`
 }
+
+export const calculateCollateralizationRatio = (collateralAmount, issuingAmount) => {
+    const issuingAmountInBaseCurrency = issuingAmount / 30
+    return (collateralAmount / issuingAmountInBaseCurrency) * 100
+}
