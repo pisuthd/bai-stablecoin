4b88744706719d92b85a8e5cd208c1ca1625a650
Pisuth D <pisuth.dae@gmail.com>
2020-12-13 00:00:08 +0700

    Feat: Implement withdraw/redeem position methods

--- contracts/PositionManager.sol
+++ contracts/PositionManager.sol
@@ -41,7 +41,11 @@ contract PositionManager is IPositionManager, Lockable {
     event NewSponsor(address indexed sponsor);
     event PositionCreated(address indexed sponsor, uint256 indexed collateralAmount, uint256 indexed tokenAmount);
     event Deposit(address indexed sponsor, uint256 indexed collateralAmount);
+    event Withdrawal(address indexed sponsor, uint256 indexed collateralAmount);
+    event EndedSponsorPosition(address indexed sponsor);
 
+
+    // FIXME: Setup minSponsorTokens to avoid too low amount of BAI tokens being minted
 
     /**
      * @param _collateralAddress currency to be used as collateral
@@ -109,7 +113,7 @@ contract PositionManager is IPositionManager, Lockable {
         require(collateralAmount > 0, "Invalid collateral amount");
         PositionData storage positionData = _getPositionData(msg.sender);
 
-        // Increase the position and global collateral balance by collateral amount.
+        // Increase the position and collateral balance by collateral amount.
         _incrementCollateralBalances(positionData, collateralAmount);
 
         emit Deposit(msg.sender, collateralAmount);
@@ -117,7 +121,53 @@ contract PositionManager is IPositionManager, Lockable {
         // Move collateral currency from sender to contract.
         // FIXME: use safeTransferFrom()
         collateralCurrency.transferFrom(msg.sender, address(this), collateralAmount);
+    }
 
+    /**
+     * @dev Reduce the collateralization level of a position
+     * @param collateralAmount total amount of collateral tokens to be sent back to the sponsor
+     */
+    function withdraw(uint256 collateralAmount) public nonReentrant() {
+        PositionData storage positionData = _getPositionData(msg.sender);
+        require(collateralAmount > 0, "Invalid collateral amount");
+
+        _decrementCollateralBalances(positionData, collateralAmount);
+        emit Withdrawal(msg.sender, collateralAmount);
+
+        // FIXME: use safeTransferFrom()
+        collateralCurrency.transfer(msg.sender, collateralAmount);
+    }
+
+    /**
+     * @dev Burns 'numTokens' and sends back to the sponsor, delete the position when all tokens have been burnt
+     * @param numTokens is the number of tokens to be burnt
+     */
+    function redeem(uint256 numTokens)
+        public
+        nonReentrant()
+    {
+        PositionData storage positionData = _getPositionData(msg.sender);
+        require(!(numTokens > positionData.tokensOutstanding), "Invalid token amount");
+
+        uint256 collateralRedeemed = _calculateCollateralRedeemed(numTokens);
+
+        if (positionData.tokensOutstanding.isEqual(numTokens)) {
+            _deleteSponsorPosition(msg.sender);
+        } else {
+            // Decrement the sponsor's collateral and global collateral amounts.
+             _decrementCollateralBalances(positionData, collateralRedeemed);
+            // uint256 newTokenCount = positionData.tokensOutstanding.sub(numTokens);
+            // require(newTokenCount > minSponsorTokens , "Below minimum sponsor position");
+            positionData.tokensOutstanding = positionData.tokensOutstanding.sub(numTokens);
+            totalTokensOutstanding = totalTokensOutstanding.sub(numTokens);
+        }
+
+        emit Redeem(msg.sender, collateralRedeemed, numTokens);
+        // Transfer collateral from contract to caller and burn callers synthetic tokens.
+        // FIXME: use safeTransferFrom()
+        collateralCurrency.transfer(msg.sender, collateralRedeemed);
+        tokenCurrency.transferFrom(msg.sender, address(this), numTokens);
+        tokenCurrency.burn(numTokens);
     }
 
     /**
@@ -195,6 +245,30 @@ contract PositionManager is IPositionManager, Lockable {
         return fullyBacked.mul(1000000).div(numTokens);
     }
 
+    function _calculateCollateralRedeemed(uint256 numTokens)
+        private
+        view
+        returns (uint256 collateralRedeemed)
+    {
+        uint256 currentRate = oracle.getValue();
+        return numTokens.div(currentRate).div(1000000);
+    }
+
+    function _deleteSponsorPosition(address sponsor) internal {
+        PositionData storage positionToLiquidate = _getPositionData(sponsor);
+
+        uint256 remainingRawCollateral = positionToLiquidate.rawCollateral;
+        rawTotalPositionCollateral = rawTotalPositionCollateral.sub(remainingRawCollateral);
+        totalTokensOutstanding = totalTokensOutstanding.sub(positionToLiquidate.tokensOutstanding);
+
+        // Reset the sponsors position to have zero outstanding and collateral.
+        delete positions[sponsor];
+
+        emit EndedSponsorPosition(sponsor);
+        // Return fee-adjusted amount of collateral deleted from position.
+        // return startingGlobalCollateral.sub(_getFeeAdjustedCollateral(rawTotalPositionCollateral));
+    }
+
     function _incrementCollateralBalances(
         PositionData storage positionData,
         uint256 collateralAmount
@@ -203,6 +277,13 @@ contract PositionManager is IPositionManager, Lockable {
         rawTotalPositionCollateral = rawTotalPositionCollateral.add(collateralAmount);
     }
 
-    
+    function _decrementCollateralBalances(
+        PositionData storage positionData,
+        uint256 collateralAmount
+    ) internal {
+        positionData.rawCollateral = positionData.rawCollateral.sub(collateralAmount);
+        require(_checkCollateralization(positionData.rawCollateral, positionData.tokensOutstanding), "Position below than collateralization ratio");
+        rawTotalPositionCollateral = rawTotalPositionCollateral.sub(collateralAmount);
+    }
 
 }
